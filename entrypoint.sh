e6defaf1c7c4fd20d55170c3a44b83b6d02022cc
sinse100 <sinse100@naver.com>
2025-07-16 17:15:48 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,9 @@
-python3 -m venv /home/ubuntu/myvenv
-source /home/ubuntu/myvenv/bin/activate
-cd /home/ubuntu/KAIA_CSMS_BETA/kaia_app
-pip3 install -r /home/ubuntu/KAIA_CSMS_BETA/kaia_app/requirements.txt
-python3 /home/ubuntu/KAIA_CSMS_BETA/kaia_app/manage.py migrate --run-syncdb
-python3 /home/ubuntu/KAIA_CSMS_BETA/kaia_app/manage.py createsuperuser --username admin --email admin@example.com --noinput
-python3 /home/ubuntu/KAIA_CSMS_BETA/kaia_app/manage.py collectstatic
-python3 /home/ubuntu/KAIA_CSMS_BETA/kaia_app/manage.py runserver 0.0.0.0:8080
+cd /home/ubuntu
+python3 -m venv myvenv
+source myvenv/bin/activate
+cd /home/ubuntu/KAIA_CSMS_BETA/KAIA_CSMS_BETA/kaia_app
+pip3 install -r requirements.txt
+python3 manage.py migrate --run-syncdb
+python3 manage.py createsuperuser --username admin --email admin@example.com --noinput
+python3 manage.py collectstatic
+python3 manage.py runserver 0.0.0.0:8080
